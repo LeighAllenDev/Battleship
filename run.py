d96f6ba76babc87c0f34fb3694b1f9343a430898
LeighAllenDev <140525595+LeighAllenDev@users.noreply.github.com>
2023-11-09 11:03:29 +0000

    import: time library

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
-
+import time
 # Global variable for the game board
 board = [[]]
 # Global variable for the game board size
