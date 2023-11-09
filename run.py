403655919001d23ba3eb9242ad132d54006e69cc
LeighAllenDev <140525595+LeighAllenDev@users.noreply.github.com>
2023-11-09 11:32:55 +0000

    feat: add function to print the game board

--- run.py
+++ run.py
@@ -12,3 +12,34 @@ number_ships = 4
 # Global variable for shots remaining
 shots_left = 50
 
+# Variable for Global alphabet
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def print_board():
+    """
+    Prints the board to the terminal
+    """
+    global board
+    global alphabet
+
+    debug_mode = True
+
+    alphabet = alphabet[0: len(board) + 1]
+
+    for row in range(len(board)):
+        print(alphabet[row], end=") ")
+        for col in range(len(board[row])):
+            if board[row][col] == "0":
+                if debug_mode:
+                    print("0", end=" ")
+                else:
+                    print(".", end=" ")
+            else:
+                print(board[row][col], end=" ")
+        print("")
+
+    print("  ", end=" ")
+    for i in range(len(board[0])):
+        print(str(i), end=" ")
+    print("")
+
